b52ff0c8359fcadeef84e5a4f82bc3884c28b470
g6r6e6g <gregoryjan@gmail.com>
2019-12-15 16:23:06 +0100

    1st stable release with UI, original motion compensation and new YawVR base motion compensation

--- client_overlay/bin/windeployqt.bat
+++ client_overlay/bin/windeployqt.bat
@@ -6,3 +6,5 @@ windeployqt.exe --dir qtdata --libdir . --plugindir qtdata/plugins --no-system-d
 
 @REM Debug:
 @REM windeployqt.exe --dir qtdata --libdir . --plugindir qtdata/plugins --no-system-d3d-compiler --compiler-runtime --no-opengl-sw --debug  --qmldir res/qml OpenVR-InputEmulatorOverlay.exe
+
+cd ..
